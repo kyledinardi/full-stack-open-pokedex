af3d1e35137e8b741878a2fb8ec30533658719c7
kyled <94251418+kyledinardi@users.noreply.github.com>
2025-06-10 13:41:21 -0400

    Add version identifier endpoint

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
+
 const app = express();
-
-// get the port from env variable
 const PORT = process.env.PORT || 5000;
-
 app.use(express.static('dist'));
+app.get('/version', (req, res) => res.send('1'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
