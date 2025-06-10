ca6df53513766d3b921a409024a2aae4fae192ac
kyled <94251418+kyledinardi@users.noreply.github.com>
2025-06-10 14:38:56 -0400

    Change version number

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.static('dist'));
-app.get('/version', (req, res) => res.send('1'));
+app.get('/version', (req, res) => res.send('2'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
